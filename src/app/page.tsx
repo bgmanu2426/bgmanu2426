7dc27a912b39e1e9a3ddb7ae774199448387b14f
manubg <bgmanu2426@gmail.com>
2024-11-20 07:37:17 +0530

    add projects page and update contact links; replace Twitter with Discord and WhatsApp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
             <div className="flex-col flex flex-1 space-y-1.5">
               <BlurFadeText
                 delay={BLUR_FADE_DELAY}
-                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                className="text-xl font-bold tracking-tighter sm:text-5xl xl:text-5xl/none"
                 yOffset={8}
                 text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
               />
@@ -206,12 +206,18 @@ export default function Page() {
                 Get in Touch
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Want to chat? Just shoot me a dm{" "}
+                Want to chat? Just shoot me a dm with a direct question on{" "}
                 <Link
-                  href={DATA.contact.social.X.url}
+                  href={DATA.contact.social.Discord.url}
                   className="text-blue-500 hover:underline"
                 >
-                  with a direct question on twitter
+                  Discord
+                </Link>{" or "}
+                <Link
+                  href={DATA.contact.social.Whatsapp.url}
+                  className="text-blue-500 hover:underline"
+                >
+                WhatsApp
                 </Link>{" "}
                 and I&apos;ll respond whenever I can. I will ignore all
                 soliciting.
